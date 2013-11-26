ebbed069b7d4accb6691280f722a8f5ac2dd0859
balajincuelogic <balaji.nadupuru@cuelogic.co.in>
2013-11-26 14:49:36 +0530

    new menu added

--- src/com/example/gitlearningsampleandroid/MainActivity.java
+++ src/com/example/gitlearningsampleandroid/MainActivity.java
@@ -17,6 +17,9 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
 		// Inflate the menu; this adds items to the action bar if it is present.
+        //add a menu
+		menu.add(1, 1, 0, "NEW");
+        
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
