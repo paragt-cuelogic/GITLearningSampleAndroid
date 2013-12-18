2f0ef7ce8fdc23723348d45c1b0e7b2c4d1e24c1
Shweta Kadu <shweta.kadu@cuelogic.co.in>
2013-12-18 15:27:50 +0530

    Added test comment.

--- src/com/example/demoapp/MainActivity.java
+++ src/com/example/demoapp/MainActivity.java
@@ -10,6 +10,10 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+
+// Test comment.
+
 	}
 	
 	@Override
